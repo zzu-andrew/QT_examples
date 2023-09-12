9f664592d383981d8f7f262177f767dd7bbd9c14
zzu-andrew <564631192@qq.com>
2023-09-12 23:44:06 +0800

    添加部分修改

--- qt01/main.cpp
+++ qt01/main.cpp
@@ -22,29 +22,29 @@ int main(int argc, char *argv[])
     win.show();
     // 添加menu
 
-    win.FileOperator();
+//    win.FileOperator();
 
-    win.menuBar()->addMenu("Edit");
-    win.menuBar()->addMenu("Tools");
-    win.menuBar()->addMenu("Help");
+//    win.menuBar()->addMenu("Edit");
+//    win.menuBar()->addMenu("Tools");
+//    win.menuBar()->addMenu("Help");
 
-    // 添加工具条
-    auto newToolBar = new QToolBar(&win);
-    newToolBar->setObjectName(QString::fromUtf8("newToolBar"));
-    win.addToolBar(Qt::RightToolBarArea, newToolBar);
-    newToolBar->addAction("paste");
+//    // 添加工具条
+//    auto newToolBar = new QToolBar(&win);
+//    newToolBar->setObjectName(QString::fromUtf8("newToolBar"));
+//    win.addToolBar(Qt::RightToolBarArea, newToolBar);
+//    newToolBar->addAction("paste");
 
-    QStringList tiplist;
-    tiplist << "Hello" << "How are you!" << "Haha" << "Oh no!";\
-    // 输入辅助
-    QCompleter *completer = new QCompleter(tiplist, &win);
-    // 设置大小写不敏感
-    completer->setCaseSensitivity(Qt::CaseInsensitive);
-    // 输入内容包含就会提示
-    completer->setFilterMode(Qt::MatchContains);
-    QLineEdit *edit = new QLineEdit(&win);
-    edit->setCompleter(completer);
-    edit->show();
+//    QStringList tiplist;
+//    tiplist << "Hello" << "How are you!" << "Haha" << "Oh no!";\
+//    // 输入辅助
+//    QCompleter *completer = new QCompleter(tiplist, &win);
+//    // 设置大小写不敏感
+//    completer->setCaseSensitivity(Qt::CaseInsensitive);
+//    // 输入内容包含就会提示
+//    completer->setFilterMode(Qt::MatchContains);
+//    QLineEdit *edit = new QLineEdit(&win);
+//    edit->setCompleter(completer);
+//    edit->show();
 
 
 //    QLabel *label = new QLabel(&win);
@@ -91,14 +91,14 @@ int main(int argc, char *argv[])
 
 
 
-    Newspaper newspaper("Biden is dog.");
-    Reader reader;
-    QObject::connect(&newspaper, &Newspaper::NewPaper, &reader, &Reader::ReceiverNewsPaper);
-    QObject::connect(&newspaper, &Newspaper::NewPaper, [=](const QString & name){
-        qDebug() << "lambda : " << name ;
-    });
+//    Newspaper newspaper("Biden is dog.");
+//    Reader reader;
+//    QObject::connect(&newspaper, &Newspaper::NewPaper, &reader, &Reader::ReceiverNewsPaper);
+//    QObject::connect(&newspaper, &Newspaper::NewPaper, [=](const QString & name){
+//        qDebug() << "lambda : " << name ;
+//    });
 
-    newspaper.Send();
+//    newspaper.Send();
 
     return app.exec();
 }
--- qt02/main.cpp
+++ qt02/main.cpp
@@ -12,41 +12,45 @@ int main(int argc, char *argv[])
     MainWindow window;
     window.show();
 
+
+
+
+
     // EventLabel测试
-    EventLabel label;
-    label.setWindowTitle("MoseEvent Demo");
-    label.resize(300, 200);
-    label.show();
+//    EventLabel label;
+//    label.setWindowTitle("MoseEvent Demo");
+//    label.resize(300, 200);
+//    label.show();
 
-    // 单独常见一个窗口，因为主窗口中已经绑定过布局了这里就不绑定了
-    QWidget mainWin;
-    // 将新窗口设置为主窗口的中心窗口
-//    window.setCentralWidget(&mainWin);
-    // 设置窗口标题
-    window.setWindowTitle("Enter your age");
-    // QSpinBox 是一个输入框并且可以通过上下按钮修改数值
-    QSpinBox *spinBox = new QSpinBox(&mainWin);
-    // 滑动句柄
-    QSlider *slider = new QSlider(Qt::Horizontal, &mainWin);
-    // 数值范围
-    spinBox->setRange(0, 130);
-    // 滑动句柄代表的数值范围
-    slider->setRange(0, 130);
-    // 以下是如何通过两个信号将输入窗口和滑动窗口之间实现梦幻联动
-    // 一旦滑动窗口值改变，将值通过信号发送给输入窗口
-    QObject::connect(slider, &QSlider::valueChanged, spinBox, &QSpinBox::setValue);
-    // 输入窗口值变动也会通知滑动窗口
-    void (QSpinBox:: *spinBoxSignal)(int) = &QSpinBox::valueChanged;
-    QObject::connect(spinBox, spinBoxSignal, slider, &QSlider::setValue);
-    spinBox->setValue(35);
-    //给控件设置布局
-    QHBoxLayout *layout = new QHBoxLayout;
-    layout->addWidget(spinBox);
-    layout->addWidget(slider);
+//    // 单独常见一个窗口，因为主窗口中已经绑定过布局了这里就不绑定了
+//    QWidget mainWin;
+//    // 将新窗口设置为主窗口的中心窗口
+////    window.setCentralWidget(&mainWin);
+//    // 设置窗口标题
+//    window.setWindowTitle("Enter your age");
+//    // QSpinBox 是一个输入框并且可以通过上下按钮修改数值
+//    QSpinBox *spinBox = new QSpinBox(&mainWin);
+//    // 滑动句柄
+//    QSlider *slider = new QSlider(Qt::Horizontal, &mainWin);
+//    // 数值范围
+//    spinBox->setRange(0, 130);
+//    // 滑动句柄代表的数值范围
+//    slider->setRange(0, 130);
+//    // 以下是如何通过两个信号将输入窗口和滑动窗口之间实现梦幻联动
+//    // 一旦滑动窗口值改变，将值通过信号发送给输入窗口
+//    QObject::connect(slider, &QSlider::valueChanged, spinBox, &QSpinBox::setValue);
+//    // 输入窗口值变动也会通知滑动窗口
+//    void (QSpinBox:: *spinBoxSignal)(int) = &QSpinBox::valueChanged;
+//    QObject::connect(spinBox, spinBoxSignal, slider, &QSlider::setValue);
+//    spinBox->setValue(35);
+//    //给控件设置布局
+//    QHBoxLayout *layout = new QHBoxLayout;
+//    layout->addWidget(spinBox);
+//    layout->addWidget(slider);
 
-    mainWin.setLayout(layout);
+//    mainWin.setLayout(layout);
 
-    window.show();
+//    window.show();
 
 
     return a.exec();
