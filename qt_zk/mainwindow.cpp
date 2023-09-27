1b29ece543ca557437135eaabd1f0b6877781445
zzu-andrew <564631192@qq.com>
2023-09-27 14:10:01 +0800

    添加目录浏览功能

--- qt_zk/mainwindow.cpp
+++ qt_zk/mainwindow.cpp
@@ -14,32 +14,26 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
 
-       QString strLibPath = ":/libs/zookeeper";
+    // 创建按钮
+       button1 = new QPushButton("切换到界面1", this);
+       button2 = new QPushButton("切换到界面2", this);
 
-       if (!QLibrary::isLibrary(strLibPath))
-       {
-           qDebug() << strLibPath << "!QLibrary::isLibrary";
-       }
+       // 创建堆叠窗口
+       stackedWidget = new QStackedWidget(this);
 
-       auto m_pLib = new QLibrary(strLibPath);
-       bool libload =false;
-       if(!m_pLib->load())   //dll加载
-       {
-           qDebug()<<"load failed";
-       }else
-       {
-           qDebug()<<"load succ";
-           libload =true;
-       }
+       // 将堆叠窗口和按钮添加到主窗口的布局中
+       QVBoxLayout* layout = new QVBoxLayout();
+       layout->addWidget(button1);
+       layout->addWidget(button2);
+       layout->addWidget(stackedWidget);
 
-//       if(!resolveFunction(m_myadd, "add"))
-//       {
-//           qDebug()<<"failed";
-//       }else
-//       {
-//           int ret = m_myadd(5,8);
-//           qDebug()<<"ret = "<<ret;
-//       }
+       QWidget* centralWidget = new QWidget(this);
+       centralWidget->setLayout(layout);
+       setCentralWidget(centralWidget);
+
+       // 连接按钮的 clicked 信号到槽函数
+       connect(button1, &QPushButton::clicked, this, &MyMainWindow::showPage1);
+       connect(button2, &QPushButton::clicked, this, &MyMainWindow::showPage2);
 
 
 }
