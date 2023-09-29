66158e16cd9cbd09a7db185ebf48abbe3baa5d5f
zzu-andrew <564631192@qq.com>
2023-09-29 12:00:36 +0800

    添加翻译文件使用说明

--- qt_I18N/main.cpp
+++ qt_I18N/main.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QTranslator>
+#include <QLocale>
+#include <QDebug>
 
 
 /*
@@ -12,7 +14,7 @@
  * 3. 函数中指定翻译文件
  * 如下主文件中所示
  * 需要注意的是，翻译需要依赖提取tr中的文本内容，因此需要翻译的地方需要使用tr处理对应的文本
- *
+ * 当然以上步骤也能经过 工具->外部->Qt语言专家->(lupdate/lrelease)来完成
 */
 
 
@@ -23,7 +25,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-
+    // 如果想做成自动根据当地语言环境来设置翻译文件，可以使用 QLocale::system().name() 来获取
+    // 这样就能根据当地语言来加载不同的翻译qm文件了
+    // qDebug() << QLocale::system().name();   // "zh_CN"
     QTranslator translator;
     translator.load("../qt_I18N/tra.qm");
     a.installTranslator(&translator);
---
+++ qt_I18N/tra.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>主窗口</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>PushButton</source>
+        <translation>按钮</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="42"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="59"/>
+        <source>New</source>
+        <translation>新建</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="12"/>
+        <source>hello Qt!</source>
+        <translation>你好QT!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="16"/>
+        <source>password</source>
+        <comment>mainwindow</comment>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>ID is %1,Name is %2</source>
+        <translation>身份证： %1,名字： %2</translation>
+    </message>
+</context>
+</TS>
