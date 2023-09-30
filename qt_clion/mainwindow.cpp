a9ee1e9097767e89ba1285811294a950914706e8
zzu-andrew <564631192@qq.com>
2023-10-01 00:18:40 +0800

    修改cmakelist文件保证在linux上能使用clion进行qt工程的开发

--- qt_clion/mainwindow.cpp
+++ qt_clion/mainwindow.cpp
@@ -5,7 +5,7 @@
 // You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
 
 #include "mainwindow.h"
-#include "ui_MainWindow.h"
+#include "ui_mainwindow.h"
 
 
 MainWindow::MainWindow(QWidget *parent) :
--- qt_painter/main.cpp
+++ qt_painter/main.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QDebug>
+#include <QGraphicsView>
 /*
  * 绘图主要依赖QPainter QPaintDevice QPaintEngine三个类
  * QPainter :用来执行绘图操作
@@ -19,6 +22,30 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+
+    // 新建场景
+    QGraphicsScene scene;
+    // 创建矩形图形项
+    QGraphicsRectItem *item = new QGraphicsRectItem(0, 0, 100, 100);
+    // 将图形项添加到场景中
+    scene.addItem(item);
+    // 输出(50, 50)点处的图形项
+    qDebug() << scene.itemAt(50, 50, QTransform());
+
+    // 为场景创建视图
+    QGraphicsView view(&scene);
+//    // 设置场景的前景色
+//    view.setForegroundBrush(QColor(255, 255, 0, 100));
+//    // 设置场景的背景图片
+//    view.setBackgroundBrush(QPixmap("../myScene/background.png"));
+
+    scene.setForegroundBrush(QColor(220, 210, 0, 100));
+    scene.setBackgroundBrush(QPixmap(":/images/achou"));
+
+    view.resize(400, 300);
+    view.show();
+
 
     return a.exec();
 }
